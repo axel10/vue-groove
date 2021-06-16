f067bf86b76b295e14bff8a0a886e15efcaf4661
axel <zgmf300@outlook.com>
2021-06-16 16:04:42 +0800

    fix: 修复无效链接

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export default {
-  coverPath: 'https://https://vc-player-file-vert.vercel.app/cover/',
-  musicPath: 'https://https://vc-player-file-vert.vercel.app/mp3convert/',
+  coverPath: 'https://vc-player-file-vert.vercel.app/cover/',
+  musicPath: 'https://vc-player-file-vert.vercel.app/mp3convert/',
   musicExt: 'mp3',
 }
 export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:5010' : 'https://api.vcollection.org'
